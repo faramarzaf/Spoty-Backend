4e508236cd0b864630b45b89768dccd5790b76ba
FaramarzAf <faramarzafzali@gmail.com>
2020-12-20 15:25:27 +0330

    initial

---
+++ src/main/java/com/spoty/backend/application/SpotyBackend/SpotyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spoty.backend.application.SpotyBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotyBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotyBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spoty/backend/application/SpotyBackend/SpotyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spoty.backend.application.SpotyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
